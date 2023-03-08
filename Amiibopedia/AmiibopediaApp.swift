a711a8eee9f6f62f401dd690fd774b1bc6232c8b
danieltorres4 <dst990502@outlook.com>
2023-03-08 13:28:04 -0600

    Initial Commit

---
+++ Amiibopedia/AmiibopediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmiibopediaApp.swift
+//  Amiibopedia
+//
+//  Created by Iván Sánchez Torres on 08/03/23.
+//
+
+import SwiftUI
+
+@main
+struct AmiibopediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
